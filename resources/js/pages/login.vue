fb5d60dd20b8ff4b291e87fbe0d73d92589478ef
CorbitaKit18 <kitcorbita.viptutorsco@gmail.com>
2025-06-11 14:42:08 +0800

    Add functionality for login page

--- resources/js/pages/login.vue
+++ resources/js/pages/login.vue
@@ -1,18 +1,37 @@
 <script setup>
 import { ref } from 'vue'
 import api from '@/axios'
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
 
+const show = () => {
+    toast.add({ severity: 'error', summary: 'Error Message', detail: 'Invalid Credentials', life: 3000 });
+};
 const email = ref('')
 const password = ref('')
-const emailError = ref('')
-const passwordError = ref('')
+const errors = ref()
 
 const handleLogin = () => {
-    api.post('login')
+
+    api.post('login', {
+        email: email.value,
+        password: password.value
+    })
+    .then(res => {
+        const token = res.data.access_token
+         api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }).catch(err => {
+       if (err.status == 422) {
+         errors.value = err.response.data.errors
+       } else {
+        show()
+       }
+    })
 }
 </script>
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <p-toast />
     <div class="w-full max-w-lg p-6 bg-white rounded-lg shadow-lg">
       <h2 class="text-2xl font-semibold mb-4 text-center">Login</h2>
 
@@ -20,9 +39,9 @@ const handleLogin = () => {
         v-model="email"
         placeholder="Email"
         class="w-full mb-3"
-        :invalid="!!emailError"
+        :invalid="errors?.email"
       />
-      <small v-if="emailError" class="text-red-500">{{ emailError }}</small>
+      <small v-if="errors?.email" class="text-red-500">{{ errors.email[0] }}</small>
 
       <p-password
         v-model="password"
@@ -31,9 +50,9 @@ const handleLogin = () => {
         class="w-full mb-3"
         :feedback="false"
         :inputStyle="{ width: '100%' }"
-        :invalid="!!passwordError"
+        :invalid="errors?.password"
       />
-      <small v-if="passwordError" class="text-red-500">{{ passwordError }}</small>
+      <small v-if="errors?.password" class="text-red-500">{{ errors.password[0] }}</small>
 
       <p-button
         label="Login"
